7e94a65473af3f822887dfd5d0f7b93ea200f612
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-03-24 15:08:35 -0600

    Linear recursion

--- scheme/exercises.scm
+++ scheme/exercises.scm
@@ -28,3 +28,106 @@
 ; Get the second item in the list (a b c d).
 (define lst5 '(a b c d))
 (car (cdr lst5))
+
+; ABSOLUTE COND
+(define (>= x y)
+    (or (> x y) (= x y))
+)
+(define (abs x)
+    (cond 
+        ((>= x 0) x)
+        (else (- x))
+    )
+)
+
+; ABSOLUTE IF
+(define (abs x)
+    (if (< x 0) 
+        (- x)
+        x
+    )
+)
+    
+; Factorial
+(define (factorial n)
+    (if (= n 1)
+        1
+        (* n (factorial (- n 1)))
+    )
+)
+
+; Recursive linear iteration factorial
+(define (factorial n)
+    (fact-iter 1 1 n)
+)
+(define (fact-iter prod counter max-count) 
+    (if (> counter max-count)
+        prod
+        (fact-iter (* counter product)
+                   (+ counter 1) 
+                   max-count))
+)
+
+; Bad fibonacci
+(define (fibonacci n)
+    (if (or (= n 0) (= n 1))
+        n
+        (+ (fibonacci (- n 1)) 
+           (fibonacci (- n 2))
+        )
+    )
+)
+
+; Iterative fibonacci
+(define (fib n)
+    (fib-iter 1 0 n)
+)
+
+(define (fib-iter a b count)
+    (if (= count 0)
+        b
+        (fib-iter (+ a b) a (- count 1))
+    )
+)
+
+; Bad exponent
+(define (bp b n)
+        (if (= n 0) 
+            1
+            (* b (bp b (- n 1)))
+        )
+)
+
+; Linear exponent
+(define (power_to b n)
+     (exp-iter  b  n  1)
+)
+(define (exp-iter  b  counter  product)
+     (if  (=  counter  0)
+           product
+           (exp-iter  b
+                      (- counter 1)
+                      (* b product))
+    )
+)
+
+; Fast exponent
+(define (crazy_function b n)
+    (if (even? n)
+        (square (expt b (/ n 2)))
+        (* b (expt b (- n 1)))
+    )
+)
+
+; Good Fast exponent
+(define (fast-exp b n) 
+    (cond 
+        [(= n 0) 1]
+        [(even? n) (square (fast-exp b (/ n 2)))]
+        [else (* b (fast-exp b (- n 1)))]
+    )
+)
+
+(define (even? n)
+    (= (remainder n 2) 0)
+)
--- scheme/theory.scm
+++ scheme/theory.scm
@@ -143,42 +143,88 @@
 
 ; Car 1.1 : returns a
 (car '(a b c d))
-
 ; Car 1.2 : returns (2 1)
 (define ls4 '((2 1) 1 2 3))
 (car ls4)
-
 ; Car 1.3 : returns (1)
 (car '((1) (2) (3) (4)))
-
 ; Car 1.4 : returns ab
 (car '(ab (cd ef) gh))
-
 ; Car 1.5 : returns ((hen cow pig))
 (car '(((hen cow pig))))
-
 ; Car 1.6 : returns a
 (car '(()))
 
 ; cdr removes first element of a list: (2 3 4)
 (cdr '(1 2 3 4))
-
 ; cdr 1.1: returns (1 2 3)
 (cdr ls4) 
-
 ; cdr 1.2: returns ((b c) (d e f))
 (cdr '(a (b c) (d e f)))
-
 ; cdr 1.3: returns ((bee hive) (wasp nest))
 (cdr '((ant hill) (bee hive) (wasp nest)))
-
 ; cdr 1.4: returns ()
 (cdr '(1))
-
 ; cdr 1.5: returns ()
 (cdr '((1 2)))
-
 ; cdr 1.6: returns ()
 (cdr '(()))
 
-; Predicates on list in Scheme
+; number 1.1: returns #t
+(number? -45.67) 
+; number 1.2: returns #t
+(number? '3) 
+; number 1.3: returns #t
+(define num 1)
+(number? num)
+; number 1.4: returns #t
+(define twelve '12) 
+(number? twelve) 
+; number 1.5: returns #f
+(number? 'twelve)
+; number 1.6: returns #t
+(number? (+ 2 3))
+; number 1.7: returns #f
+(number? #t) 
+; number 1.8: returns #t
+(number? (car '(15.3 -31.7))) 
+; number 1.9: returns #f
+(number? (cdr '(15.3 -31.7))) 
+
+; symbol 1.1: returns #f
+(symbol? 15) 
+; symbol 1.2: returns #f
+(symbol? num) 
+; symbol 1.3: returns #t
+(symbol? 'num) 
+; symbol 1.4: returns #f
+(symbol? twelve) 
+; symbol 1.5: returns #t
+(symbol? 'twelve) 
+; symbol 1.6: returns #f
+(symbol? #f) 
+; symbol 1.7: returns #t
+(symbol? (car '(banana cream))) 
+; symbol 1.8: returns #f
+(symbol? (cdr ' (banana cream))) 
+
+; BOOLEAN
+(boolean? #t) ;returns #t
+(boolean? (number? 'a)) ;returns #t
+(boolean? (cons 'a '())) ;returns #f
+
+; PAIR
+(pair? '(Ann Ben Carl)) ;returns t
+(pair? '(1)) ;returns t
+(pair? '()) ;returns f
+(pair? '(())) ;returns t
+(pair? '(a (b c) d)) ;returns t
+(pair? (cons 'a '())) ;returns t
+(pair? (cons 3 4)) ;returns t
+(pair? 'pair) ;returns f
+
+; NULL
+(null? '()) ; t
+(null? (cdr '(cat))) ;t
+(null? (car '((ab)))) ;f
+
