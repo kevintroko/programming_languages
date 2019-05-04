2c28e1f516a31b5eea423fc9ea9309bff4ff80cf
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-05-03 20:14:26 -0500

    Assigment 03 code

--- prolog/assigmen03_DFA.pl
+++ prolog/assigmen03_DFA.pl
@@ -1,9 +1,19 @@
+delta(0, a, 1).
+delta(0, b, 0).
+delta(1, a, 1).
+delta(1, b, 2).
+delta(2, a, 2).
+delta(2, b, 2).
+
+start(0).
+final(2).
+
 % (a, b) * ab (a, b).
 
 parse(L):- 
     start(S),
     trans(S,L).
-trans(X, [A|B]) :-
+trans(X,[A|B]) :-
     delta(X, A, Y), /* X---A--->Y*/
     write(X),
     write(' '),
